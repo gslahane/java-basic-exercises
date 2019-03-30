5b5df127406045bde633d05e07be6eb8d90383c8
Gangadhar Lahane <=>
2019-03-30 12:52:06 +0530

    Initival commit

---
+++ PrintDayInWords.java
@@ -0,0 +1,38 @@
+/*
+* Java program to print day in words
+* Using switch statement
+*/
+
+class PrintDayInWords {
+
+	public static void main(String[] args) {
+		
+		int day = 10;
+
+		switch(day) {
+			case 1:
+				System.out.println("Monday");
+				break;
+			case 2:
+				System.out.println("Tuesday");
+				break;
+			case 3:
+				System.out.println("Wednesday");
+				break;
+			case 4:
+				System.out.println("Thursday");
+				break;
+			case 5:
+				System.out.println("Friday");
+				break;
+			case 6:
+				System.out.println("Saturday");
+				break;
+			case 7:
+				System.out.println("Sunday");
+				break;
+			default:
+				System.out.println("Invalid week day no!");
+		}
+	}
+}
